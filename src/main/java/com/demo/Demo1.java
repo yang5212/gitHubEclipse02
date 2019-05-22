14fac57fb05f607e55a1868b0288088a0e481901
tom_pro <tomkk@dd.com>
2019-05-22 19:52:06 +0800

    下课了

--- src/main/java/com/demo/Demo1.java
+++ src/main/java/com/demo/Demo1.java
@@ -3,10 +3,6 @@ package com.demo;
 public class Demo1 {
 	public static void main(String[] args) {
 		System.out.println("你好啊");
-		System.out.println("你好");
-		System.out.println("你好");
-		System.out.println("你好");
-		
 		System.out.println("你好 啊");
 	}
 
