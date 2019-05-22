d8eec90c0bacb86d42019bdbd926fdac5eafa194
tom_pro <tomkk@dd.com>
2019-05-22 19:53:59 +0800

    嗯 吃饭吧

--- src/main/java/com/demo/Demo1.java
+++ src/main/java/com/demo/Demo1.java
@@ -4,8 +4,6 @@ public class Demo1 {
 	public static void main(String[] args) {
 		System.out.println("你好啊");
 		System.out.println("你好");
-		System.out.println("你好");
-		System.out.println("你好");
 		
 		System.out.println("你好 啊");
 		System.out.println("你好 啊");
