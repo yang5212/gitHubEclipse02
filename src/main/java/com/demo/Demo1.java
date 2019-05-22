e884bdfb4e47269701f5c142a9c45536b3340013
tom_pro <tomkk@dd.com>
2019-05-22 19:39:38 +0800

    没有啊

--- src/main/java/com/demo/Demo1.java
+++ src/main/java/com/demo/Demo1.java
@@ -3,6 +3,9 @@ package com.demo;
 public class Demo1 {
 	public static void main(String[] args) {
 		System.out.println("你好啊");
+		System.out.println("你好");
+		System.out.println("你好");
+		System.out.println("你好");
 		
 	}
 
